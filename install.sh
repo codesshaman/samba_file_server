f864a753c43618d7914edfe0534b2cfdb37679a0
Nick <90702473+codesshaman@users.noreply.github.com>
2022-08-26 06:53:02 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ apt update && apt install -y samba samba-client
 mv /etc/samba/smb.conf /etc/samba/smb.conf_backup
 mkdir /media/samba
 mkdir /media/samba/public
-chmod -R 0755 /media/samba/public
 mkdir /media/samba/private
 groupadd smbgrp
 useradd user1
@@ -14,6 +13,7 @@ cp smb.conf /etc/samba/
 testparm -s
 service smbd restart
 service smbd status
+chmod -R 0777 /media/samba/public
 iptables -A INPUT -p tcp -m tcp --dport 445 -s 10.0.0.0/24 -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 139 -s 10.0.0.0/24 -j ACCEPT
 iptables -A INPUT -p udp -m udp --dport 137 -s 10.0.0.0/24 -j ACCEPT
